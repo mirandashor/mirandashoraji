56ecd0d22fab6230b750ba7cf14b28d90d8a6e6c
mirandashor <mirandashoraji@oakland.edu>
2026-05-29 01:39:01 -0400

    added css files + import jsx to App

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,3 +1,9 @@
+import About from "./sections/About";
+import Contact from "./sections/Contact";
+import Hero from "./sections/Hero";
+import Projects from "./sections/Projects";
+import Skills from "./sections/Skills";
+
 
 function App() {
     return (
